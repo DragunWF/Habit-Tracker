23976027b5b4e1cc6028a5a1629ce64c1fbf7b8a
Marc Plarisan <plarisanmarc2@gmail.com>
2024-05-24 11:42:14 +0800

    Update session data

--- app/src/main/java/com/example/habit_tracker/utils/SessionData.java
+++ app/src/main/java/com/example/habit_tracker/utils/SessionData.java
@@ -2,8 +2,19 @@ package com.example.habit_tracker.utils;
 
 import com.example.habit_tracker.data.User;
 
+import java.util.List;
+
 public class SessionData {
     private static User currentUser;
+    private static List<User> users;
+    private static boolean initialize = false;
+
+    public static void initialize() {
+        if (!initialize) {
+            initialize = true;
+            // TODO: Get users from database and add them to users list
+        }
+    }
 
     public static User getCurrentUser() {
         return currentUser;
@@ -11,5 +22,13 @@ public class SessionData {
 
     public static void setCurrentUser(User currentUser) {
         SessionData.currentUser = currentUser;
+    }
+
+    public static List<User> getUsers() {
+        return users;
+    }
+
+    public static void setUsers(List<User> users) {
+        SessionData.users = users;
     }
 }
