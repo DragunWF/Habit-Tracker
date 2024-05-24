39fe6033e88744d2fcf339bc4521b0fdad70c15f
Marc Plarisan <plarisanmarc2@gmail.com>
2024-05-24 12:12:23 +0800

    Add recycler view adapter

---
+++ app/src/main/java/com/example/habit_tracker/utils/HabitRecycler.java
@@ -0,0 +1,74 @@
+package com.example.habit_tracker.utils;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.habit_tracker.R;
+import com.example.habit_tracker.data.Habit;
+
+import java.util.List;
+
+public class HabitRecycler extends RecyclerView.Adapter<HabitRecycler.ViewHolder> {
+
+    private List<Habit> habits;
+
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder)
+     */
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // private final TextView textView;
+
+        public ViewHolder(View view) {
+            super(view);
+            // Define click listener for the ViewHolder's View
+
+            // textView = (TextView) view.findViewById(R.id.textView);
+        }
+
+        public TextView getTextView() {
+            return textView;
+        }
+    }
+
+    /**
+     * Initialize the dataset of the Adapter
+     *
+     * @param dataSet String[] containing the data to populate views to be used
+     * by RecyclerView
+     */
+    public HabitRecycler(String[] dataSet) {
+        localDataSet = dataSet;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        // Create a new view, which defines the UI of the list item
+        //View view = LayoutInflater.from(viewGroup.getContext())
+        //        .inflate(R.layout.text_row_item, viewGroup, false);
+
+        // return new ViewHolder(view);
+        return null;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+        // viewHolder.getTextView().setText(localDataSet[position]);
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return habits.size();
+    }
+}
+
